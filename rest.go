00e5855038f8266a6dd9338c0f50a1512440c440
Jan Semmelink <jansemmelink@conor.co.za>
2019-07-31 09:41:02 +0200

    Update

--- rest.go
+++ rest.go
@@ -14,6 +14,8 @@ import (
 	"github.com/jansemmelink/log"
 )
 
+var reservedParamNames = []string{"size"}
+
 //New creates a blank API
 func New() IApi {
 	return &api{
@@ -33,6 +35,11 @@ type IApi interface {
 type api struct {
 	*pat.Router
 	items map[string]apiItem
+}
+
+type apiItem struct {
+	name  string
+	store items.IStore
 }
 
 //Add an item to the REST-full API
@@ -64,19 +71,29 @@ type itemOperResult struct {
 }
 
 func (a api) UnknownHandler(res http.ResponseWriter, req *http.Request) {
-	log.Debugf("?...")
-	http.Error(res, "Unknown path", http.StatusNotFound)
-}
+	http.Error(res, fmt.Sprintf("Unknown request %s %s", req.Method, req.URL.Path), http.StatusNotFound)
+} //api.UnknownHandler()
 
 func (a api) TmplHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
-	log.Debugf("Tmpl...")
+	if req.Method != http.MethodGet {
+		http.Error(res, "Use HTTP method GET to retrieve the template item", http.StatusBadRequest)
+		return
+	}
+
+	log.Debugf("Tmpl %s", store.Name())
 	item := store.Tmpl()
 	itemJSON, _ := json.Marshal(item)
 	res.Write(itemJSON)
 	return
-}
+} //api.TmplHandler()
 
 func (a api) AddHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Use HTTP method POST to add a new item", http.StatusBadRequest)
+		return
+	}
+
+	log.Debugf("Add %s", store.Name())
 	newItem, err := a.BodyItem(req, store)
 	if err != nil || newItem == nil {
 		reason := fmt.Sprintf("Cannot process %s data from request body: %v", store.Name(), err)
@@ -95,11 +112,16 @@ func (a api) AddHandler(res http.ResponseWriter, req *http.Request, name string,
 	resultJSON, _ := json.Marshal(result)
 	res.Write(resultJSON)
 	return
-} //NewHandler()
+} //api.AddHandler()
 
 func (a api) GetHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
+	if req.Method != http.MethodGet {
+		http.Error(res, "Use HTTP method GET to retrieve an item", http.StatusBadRequest)
+		return
+	}
+
 	id := req.URL.Query().Get(":id")
-	log.Debugf("Get(%s): id=\"%s\"", name, id)
+	log.Debugf("Get %s.id=\"%s\"", name, id)
 	item, err := store.Get(id)
 	if err != nil {
 		log.Errorf("Cannot get id=%s: %v", id, err)
@@ -109,107 +131,152 @@ func (a api) GetHandler(res http.ResponseWriter, req *http.Request, name string,
 	itemJSON, _ := json.Marshal(item)
 	res.Write(itemJSON)
 	return
-}
+} //api.GetHandler()
 
 func (a api) UpdHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
-	// id := req.URL.Query().Get(":id")
-	// log.Debugf("Upd(%s): id=\"%s\"", name, id)
-	// 	//parse new JSON value
-	// 	var value interface{}
-	// 	if err := json.NewDecoder(req.Body).Decode(&value); err != nil {
-	// 		http.Error(res, "Invalid JSON data posted", http.StatusBadRequest)
-	// 		return
-	// 	}
+	if req.Method != http.MethodPut {
+		http.Error(res, "Use HTTP method PUT to update an item", http.StatusBadRequest)
+		return
+	}
 
-	// 	existingItem, err := ai.store.Get(id)
-	// 	if err == nil {
-	// 		updatedItem, err := existingItem.Set(value)
-	// 		if err != nil {
-	// 			reason := fmt.Sprintf("Failed to set %s.id=%s: %s", ai.name, id, err)
-	// 			http.Error(res, reason, http.StatusServiceUnavailable)
-	// 			return
-	// 		}
+	id := req.URL.Query().Get(":id")
+	log.Debugf("Upd %s.id=\"%s\"", name, id)
 
-	// 		//updated in memory: save to file
-	// 		log.Debugf("upd: id=%s,rev=%d,v=%+v   (BUT NOT SAVED)",
-	// 			updatedItem.ID(),
-	// 			updatedItem.Rev(),
-	// 			updatedItem.Value())
-	// 		return
-	// 	} //if got existing item
-
-	// 	reason := fmt.Sprintf("Failed to get %s.id=%s: %v", ai.name, id, err)
-	// 	http.Error(res, reason, http.StatusServiceUnavailable)
-	// 	return
-
-}
+	updItem, err := a.BodyItem(req, store)
+	if err != nil || updItem == nil {
+		reason := fmt.Sprintf("Cannot process %s data from request body: %v", store.Name(), err)
+		http.Error(res, reason, http.StatusNotAcceptable)
+		return
+	}
+	err = store.Upd(id, updItem)
+	if err != nil {
+		reason := fmt.Sprintf("Failed to update %s.id=%s: %v", store.Name(), id, err)
+		http.Error(res, reason, http.StatusNotAcceptable)
+		return
+	}
+	//empty response on success
+	return
+} //api.UpdHandler()
 
 func (a api) DelHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
-	// 	err := ai.store.Del(id)
-	// 	if err == nil {
-	// 		log.Debugf("deleted: id=%s", id)
-	// 		return
-	// 	} //if got existing item
-	// 	reason := fmt.Sprintf("Failed to del %s.id=%s: %v", ai.name, id, err)
-	// 	http.Error(res, reason, http.StatusNotFound)
-	// 	return
+	if req.Method != http.MethodDelete {
+		http.Error(res, "Use HTTP method DELETE to delete an item", http.StatusBadRequest)
+		return
+	}
 
-	// } //switch(method)
-	// http.Error(res, fmt.Sprintf("Expecting GET|PUT|POST method"), http.StatusMethodNotAllowed)
-	// return
+	id := req.URL.Query().Get(":id")
+	log.Debugf("Del %s.id=\"%s\"", name, id)
+
+	err := store.Del(id)
+	if err != nil {
+		reason := fmt.Sprintf("Failed to delete %s.id=%s: %v", store.Name(), id, err)
+		http.Error(res, reason, http.StatusNotFound)
+		return
+	}
+	//empty response on success
+	return
 }
 
 func (a api) ListHandler(res http.ResponseWriter, req *http.Request, name string, store items.IStore) {
+	if req.Method != http.MethodGet && req.Method != http.MethodPost {
+		http.Error(res, "Use HTTP method GET or POST to retrieve the list", http.StatusBadRequest)
+		return
+	}
+
 	size, err := strconv.Atoi(req.URL.Query().Get("size"))
 	if err != nil {
 		size = 10
 	}
-	var filterItem items.IItem
-	switch req.Method {
-	case http.MethodPost:
-		//POST parses optional body as item filter
-		var err error
-		filterItem, err = a.BodyItem(req, store)
-		if err != nil {
-			reason := fmt.Sprintf("Cannot process %s data as filter from request body: %v", store.Name(), err)
-			http.Error(res, reason, http.StatusBadRequest)
-			return
-		}
-
-	case http.MethodGet:
-		//GET parses optional params are item fields
-		//todo:...
+	filterItem, err := a.BodyItem(req, store)
+	if err != nil {
+		reason := fmt.Sprintf("Cannot process %s filter from request: %v", store.Name(), err)
+		http.Error(res, reason, http.StatusBadRequest)
+		return
 	}
 
-	log.Debugf("List... filter=%T=%+v", filterItem, filterItem)
 	itemList := store.Find(size, filterItem)
-	log.Debugf("Got %d items: %v", len(itemList), itemList)
+	log.Debugf("List %s.{size=%d,filter=%v} -> %d: %v", store.Name(), size, filterItem, len(itemList), itemList)
+
 	jsonList, _ := json.Marshal(itemList)
 	res.Write(jsonList)
-}
+} //api.ListHandler()
 
-type apiItem struct {
-	name string
-	//tmpl  interface{}
-	store items.IStore
-}
-
+//BodyItem parses the request body as a store item
+//then apply any URL parameters on top of that, overwriting the body attributes
+//(it does not do validation because filter items do not have to be valid)
 func (a api) BodyItem(req *http.Request, store items.IStore) (items.IItem, error) {
-	newItemDataPtrValue := reflect.New(store.Type())
-	newItemDataPtr := newItemDataPtrValue.Interface()
-	err := json.NewDecoder(req.Body).Decode(newItemDataPtr)
-	if err == io.EOF {
-		return nil, nil
-	}
-	if err != nil {
-		return nil, log.Wrapf(err, "failed to parse JSON object for %s: %v", store.Name(), err)
-	}
-	newItem, ok := newItemDataPtr.(items.IItem)
+	//create a new item in memory
+	itemDataPtrValue := reflect.New(store.Type())
+	itemDataPtr := itemDataPtrValue.Interface()
+	newItem, ok := itemDataPtr.(items.IItem)
 	if !ok {
-		return nil, log.Wrapf(err, "failed to convert %T to %s", newItemDataPtr, store.Name())
+		return nil, log.Wrapf(nil, "failed to create new item from store.type=%v", store.Type())
 	}
-	if err := newItem.Validate(); err != nil {
-		return nil, log.Wrapf(err, "invalid %s: %v", store.Name(), err)
+
+	//decode the (optional) request body into the item
+	err := json.NewDecoder(req.Body).Decode(itemDataPtr)
+	if err != nil && err != io.EOF {
+		return nil, log.Wrapf(err, "request body is not valid JSON data for %s", store.Name())
 	}
+
+	// if err == nil {
+	// 	log.Debugf("Item from body: %T: %+v", itemDataPtr, itemDataPtr)
+	// } //if read item from request body
+
+	//process URL parameters to overwrite item struct fields
+	structType := store.Type()
+	if structType.Kind() == reflect.Ptr {
+		structType = structType.Elem()
+	}
+	if structType.Kind() == reflect.Struct {
+		//see if this param is an store item JSON attribute
+		setParamCount := 0
+		for index := 0; index < structType.NumField(); index++ {
+			fieldType := structType.Field(index)
+			fieldName := fieldType.Name
+			if len(fieldType.Tag.Get("json")) > 0 {
+				fieldName = fieldType.Tag.Get("json")
+			}
+			//skip reserved names used in REST interface
+			reserved := false
+			for _, reservedName := range reservedParamNames {
+				if fieldName == reservedName {
+					log.Debugf("Not applying URL param %s with reserved name as item attr.", fieldName)
+					reserved = true
+					break
+				}
+			} //for each reserved name
+
+			if !reserved {
+				stringValue := req.URL.Query().Get(fieldName)
+				if len(stringValue) > 0 {
+					//field value is specified in params
+					fieldValue := itemDataPtrValue.Elem().Field(index)
+					if reflect.TypeOf(stringValue).AssignableTo(fieldType.Type) {
+						//can assign string value
+						fieldValue.Set(reflect.ValueOf(stringValue))
+						setParamCount++
+					} else {
+						//cannot assign string value, try int
+						intValue, err := strconv.Atoi(stringValue)
+						if err == nil {
+							//param has int value
+							if reflect.TypeOf(intValue).AssignableTo(fieldType.Type) {
+								//can assign int value
+								fieldValue.Set(reflect.ValueOf(intValue))
+								setParamCount++
+							} else {
+								return nil, log.Wrapf(nil, "URL param %s=%s cannot be used to filter. Only top level int and string attributes can be used", fieldName, stringValue)
+							} //if cannot assign as int
+						} //if got int value
+					} //if cannot assign as string
+				} //if value specified
+			} //if not reserved name
+		} //for each struct.field[]
+
+		// if setParamCount > 0 {
+		// 	log.Debugf("Item after applying attrs: %T: %+v", itemDataPtr, itemDataPtr)
+		// }
+	} //if has struct type for store item
 	return newItem, nil
 }
--- user/main.go
+++ user/main.go
@@ -13,25 +13,41 @@ import (
 
 func main() {
 	log.DebugOn()
-	userStore := jsonfiles.MustNew("./users", "user", user{})
+	userStore := jsonfiles.MustNew("./share", "user", user{})
 	api := rest.New().WithItem(userStore)
 	http.ListenAndServe("localhost:8000", api)
 }
 
 type user struct {
-	Name string
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 func (u user) Validate() error {
+	if len(u.Name) == 0 {
+		return log.Wrapf(nil, "user.name not specified")
+	}
+	if u.Age < 0 {
+		return log.Wrapf(nil, "user.age is negative")
+	}
 	return nil
 }
 
+//Match is used to filter in a list
+//it is simple, not doing range checks, just substring on name and exact value of age
+//todo: add a filter struct type to support like age ranges etc...
 func (u user) Match(filter items.IItem) error {
 	uf := filter.(*user)
 	if len(uf.Name) > 0 {
-		//look for sub-match of filter anywhat in name
+		//look for sub-match of filter anywhere in name
 		if strings.Index(u.Name, uf.Name) == -1 {
-			return log.Wrapf(nil, "name mismatch")
+			return log.Wrapf(nil, "name mismatch (%s,%s)", u.Name, uf.Name)
+		}
+	}
+	if uf.Age != 0 {
+		//look for sub-match of age
+		if u.Age != uf.Age {
+			return log.Wrapf(nil, "age mismatch(%d,%d)", u.Age, uf.Age)
 		}
 	}
 	return nil
